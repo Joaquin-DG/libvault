094492846234e13a6448b53ee934f22933af32f3
Joaquin-DG <fuencalientejoaquin@gmail.com>
2026-07-22 20:36:05 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+
+
+#define CLAY_IMPLEMENTATION
+
+#include <clay.h>
+#include <iostream>
+#include <raylib.h>
+
+int main ( int argc , char *argv[])
+{ 
+
+    std::cout << "hola caracola" << std::endl;
+
+    return 0;
+
+}
